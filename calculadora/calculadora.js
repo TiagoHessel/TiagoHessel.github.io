e03d6098f20f56ba4a4a36e72a4b4e662be8de67
Tiago Hessel <71556666+TiagoHessel@users.noreply.github.com>
2021-01-31 17:31:00 -0300

    Calculadora
    Criando Calculadora Simples utilizando o JS + HTML5 + CSS3

---
+++ calculadora/calculadora.js
@@ -0,0 +1,13 @@
+var valor;
+function botao(num) {
+    valor = document.calc.visor.value += num;
+}
+
+function reseta(){
+    document.calc.visor.value = '';
+}
+
+function calcula(){
+    resultado = eval(valor);
+    document.calc.visor.value = resultado.toLocaleString('pt-br');
+}
